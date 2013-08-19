ba4097497f8f9cac57d25f4d53fdb03e76e270f1
Insect of web <webuffer@foxmail.com>
2013-08-19 10:55:49 +0800

    A wordPress theme uses bootstrap.

---
+++ footer.php
@@ -0,0 +1,11 @@
+		<hr>
+
+		<footer>
+			<p>&copy; Company 2012</p>
+		</footer>
+	</div><!-- /container -->
+
+	<!-- Placed at the end of the document so the pages load faster -->
+	<?php wp_footer(); ?>
+</body>
+</html>
